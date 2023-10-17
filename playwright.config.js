1e9f265a88d1249aceea014ef17e1706fb9b7472
enzofurlan <enzoofurlan@gmail.com>
2023-10-16 21:32:02 -0300

    - Playwright course;

---
+++ playwright.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+const { defineConfig, devices } = require("@playwright/test");
+
+module.exports = defineConfig({
+  testDir: "./tests",
+  reporter: "html",
+  retries: 0,
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
